f42fc5da9df9fc093cd1d367c59b70b44ad9979e
Carlosrvda <carlosrvda@gmail.com>
2022-09-30 23:27:52 -0500

    Paginas SignUp

---
+++ djangoCRUD/tasks/views.py
@@ -0,0 +1,9 @@
+from turtle import title
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def helloworld(request):
+    return render(request, 'signup.html', {
+        'form': UserCreationForm
+    })
