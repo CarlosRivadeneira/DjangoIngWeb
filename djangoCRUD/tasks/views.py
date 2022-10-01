428ab77628a080502a750dae4b5cd3d6036f7e9d
Carlosrvda <carlosrvda@gmail.com>
2022-10-01 00:25:12 -0500

    RegistrarUsuarios

--- djangoCRUD/tasks/views.py
+++ djangoCRUD/tasks/views.py
@@ -1,9 +1,30 @@
 from turtle import title
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        print('Enviando formulario')
+        return render(request, 'signup.html',{
+        'form' : UserCreationForm
+        })
+    else:
+        print(request.POST)
+        print('Obteniendo datos')
+        if request.POST['password1'] == request.POST['password2']:
+            #register User
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.
+                POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado exitosamente')
+            except:
+                return HttpResponse('El nombre de usuario ya existe')
+        return HttpResponse('Las contraseñas no coinciden')
