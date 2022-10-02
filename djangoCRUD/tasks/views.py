0aabd8f57d80974fee04503b4aa4f352fe1258ff
Carlosrvda <carlosrvda@gmail.com>
2022-10-02 00:51:54 -0500

    LoginLogout

--- djangoCRUD/tasks/views.py
+++ djangoCRUD/tasks/views.py
@@ -2,9 +2,9 @@ from cmath import log
 from distutils.log import error
 from turtle import title
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 from django.db import IntegrityError
 
@@ -43,3 +43,25 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html',{
+            'form':AuthenticationForm
+        })
+    else:
+        print(request.POST)
+        user = authenticate(request, username=request.POST['username'], password=request.POST
+            ['password'])
+        if user is None:
+            return render(request, 'signin.html',{
+                'form':AuthenticationForm,
+                'error': 'El nombre de usuario o contraseña están incorrectos'
+            })  
+        else:
+            login(request, user)
+            return redirect('tasks')
