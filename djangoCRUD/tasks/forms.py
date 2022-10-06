90ced5ab8e4ce5ec31739fe508c3129f00b09e66
Carlosrvda <carlosrvda@gmail.com>
2022-10-06 10:30:46 -0500

    Bootstrap

--- djangoCRUD/tasks/forms.py
+++ djangoCRUD/tasks/forms.py
@@ -2,8 +2,14 @@ from dataclasses import fields
 from pyexpat import model
 from django.forms import ModelForm
 from .models import Task
+from django import forms
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe un título'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escribe una descripción'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'})
+        }
